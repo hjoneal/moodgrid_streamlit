6b8710e33480a89f179f431d7801b2c3fbb9bb58
Harry Neal <harryjoneal@gmail.com>
2023-05-26 18:41:37 +0100

    trying overhauled permissions script

--- moodgrid_webapp_permissions.py
+++ moodgrid_webapp_permissions.py
@@ -647,24 +647,137 @@ if button("Get started", key="button1"):
             # CLIENT_ID = os.getenv("CLIENT_ID", "")
             # CLIENT_SECRET = os.getenv("CLIENT_SECRET", "")
 
-            # authenticate
-            client_credentials_manager = SpotifyClientCredentials(
-                client_id=CLIENT_ID, client_secret=CLIENT_SECRET
-            )
+            def get_token(oauth, code):
 
-            sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
-                                                        client_secret=CLIENT_SECRET,
-                                                        redirect_uri="http://localhost:8888/callback",
-                                                        scope="playlist-modify-private"))
+                token = oauth.get_access_token(code, as_dict=False, check_cache=False)
+                # remove cached token saved in directory
+                os.remove(".cache")
+    
+                # return the token
+                return token
+            
+            def sign_in(token):
+                sp = spotipy.Spotify(auth=token)
+                return sp
+            
+            def app_get_token():
+                try:
+                    token = get_token(st.session_state["oauth"], st.session_state["code"])
+                except Exception as e:
+                    st.error("An error occurred during token retrieval!")
+                    st.write("The error is as follows:")
+                    st.write(e)
+                else:
+                    st.session_state["cached_token"] = token
+
+            def app_sign_in():
+                try:
+                    sp = sign_in(st.session_state["cached_token"])
+                except Exception as e:
+                    st.error("An error occurred during sign-in!")
+                    st.write("The error is as follows:")
+                    st.write(e)
+                else:
+                    st.session_state["signed_in"] = True
+                    app_display_welcome()
+                    st.success("Sign in success!")
+                    
+                return sp
+            
+            def app_display_welcome():
+    
+                # import secrets from streamlit deployment
+                cid = st.secrets["CLIENT_ID"]
+                csecret = st.secrets["CLIENT_SECRET"]
+                uri = "http://localhost:8888/callback"
+
+                # set scope and establish connection
+                scopes = " ".join(["playlist-modify-private",
+                                "playlist-modify-public"])
+
+                # create oauth object
+                oauth = SpotifyOAuth(scope=scopes,
+                                    redirect_uri=uri,
+                                    client_id=cid,
+                                    client_secret=csecret)
+                # store oauth in session
+                st.session_state["oauth"] = oauth
+
+                # retrieve auth url
+                auth_url = oauth.get_authorize_url()
+
+                # this SHOULD open the link in the same tab when Streamlit Cloud is updated
+                # via the "_self" target
+                link_html = " <a target=\"_self\" href=\"{url}\" >{msg}</a> ".format(
+                    url=auth_url,
+                    msg="Click me to authenticate!"
+                )
+                                
+                # define welcome
+                welcome_msg = """
+                Welcome! :wave: This app uses the Spotify API to interact with general 
+                music info and your playlists! In order to view and modify information 
+                associated with your account, you must log in. You only need to do this 
+                once.
+                """
+                if not st.session_state["signed_in"]:
+                    st.markdown(welcome_msg)
+                    st.write(" ".join(["No tokens found for this session. Please log in by",
+                                    "clicking the link below."]))
+                    st.markdown(link_html, unsafe_allow_html=True)
+
+        if "signed_in" not in st.session_state:
+            st.session_state["signed_in"] = False
+        if "cached_token" not in st.session_state:
+            st.session_state["cached_token"] = ""
+        if "code" not in st.session_state:
+            st.session_state["code"] = ""
+        if "oauth" not in st.session_state:
+            st.session_state["oauth"] = None
+
+
+        # get current url (stored as dict)
+        url_params = st.experimental_get_query_params()
+
+        # attempt sign in with cached token
+        if st.session_state["cached_token"] != "":
+            sp = app_sign_in()
+        # if no token, but code in url, get code, parse token, and sign in
+        elif "code" in url_params:
+            # all params stored as lists, see doc for explanation
+            st.session_state["code"] = url_params["code"][0]
+            app_get_token()
+            sp = app_sign_in()
+        # otherwise, prompt for redirect
+        else:
+            app_display_welcome()
+
+        # only display the following after login
+        ### is there another way to do this? clunky to have everything in an if:
+        if st.session_state["signed_in"]:
+            user = sp.current_user()
+            name = user["display_name"]
+            username = user["id"]
+
+
+            # # authenticate
+            # client_credentials_manager = SpotifyClientCredentials(
+            #     client_id=CLIENT_ID, client_secret=CLIENT_SECRET
+            # )
+
+            # sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+            #                                             client_secret=CLIENT_SECRET,
+            #                                             redirect_uri="http://localhost:8888/callback",
+            #                                             scope="playlist-modify-private"))
 
             
             
-            user = sp.current_user()["id"]
+            # user = sp.current_user()["id"]
 
             
             playlist_description = f"A playlist created using MoodGrid from {plname_list[:]} with a Happy rating of {find_happy} and an Energy rating of {find_energy}"
 
-            playlist = sp.user_playlist_create(user, new_playlist_name, public=False, description=playlist_description)
+            playlist = sp.user_playlist_create(user=username, name=new_playlist_name, public=False, description=playlist_description)
 
             
             # split track_id_list into batches of 100, as Spotify only lets you add 100 tracks at a time
